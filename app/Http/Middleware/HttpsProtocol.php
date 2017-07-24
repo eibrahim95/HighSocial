3a482b7b1a053ac0c1bfb83a603121b148627f91
ibrahim E.Gad <eibrahim95@gmail.com>
2017-07-25 00:48:57 +0200

    https middleware

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,6 +16,7 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') {
+                $request->setTrustedProxies( [ $request->getClientIp() ] );
                 return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
