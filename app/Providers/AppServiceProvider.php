276288fb5a51e018ace705915ab284e585402b07
ibrahim E.Gad <eibrahim95@gmail.com>
2017-07-25 00:45:36 +0200

    https middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Facebook\Facebook;
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        \URL::forceScheme('https');
+        URL::forceScheme('https');
         $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local');
     }
 
