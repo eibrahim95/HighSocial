ab00451bce289c175991deba42088e1cdffe888f
ibrahim E.Gad <eibrahim95@gmail.com>
2017-07-25 00:41:45 +0200

    https middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        \URL::forceSchema('https');
+        \URL::forceScheme('https');
         $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local');
     }
 
