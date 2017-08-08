5777b7dc51a4bb2299ca928f34931faafce40929
ibrahim E.Gad <eibrahim95@gmail.com>
2017-08-08 22:17:28 +0200

    added privacy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-<h1> Our Prvacy Policy </h1>
+<h1> Our Privacy Policy </h1>
 <br>
 <div class='innerText'>This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.<br></div>
 <span id='infoCo'></span><br>
