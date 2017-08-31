18bee2ec2d6057316b384d147430ee02e27a0ea4
ibrahim E.Gad <eibrahim95@gmail.com>
2017-08-31 13:10:12 +0200

    hi

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,11 +62,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        
+        $user= User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+        $additional = $this->createAdditional($user->id);
+        return $user;
     }
     protected function createAdditional($user_id)
     {
